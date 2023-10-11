edf0a8cd9440154e08407e2e93d9afd04d195edb
tab4444 <thiagoblock445@gmail.com>
2023-10-10 21:18:21 -0300

    arreglos

--- pre-entrega1/main.js
+++ pre-entrega1/main.js
@@ -1,24 +1,25 @@
-// ESTOS PUNTOS DE TAREA COMPRENDEN TODO LO VISTO DURANTE LAS CLASE 1, 2, 3, 4
-// EL PRIMER DESAFÍO COMPRENDE: VARIABLES, CONDICIONALES, BUCLES, FUNCIONES
-// ESO SIGNIFICA QUE TODO EJERCICIO QUE CONTENGA MÉTODOS O DATOS QUE NO FUERON EN CLASES, NO SERÁN EVALUADOS
-// RECUERDEN QUE EL PRIMER DESAFÍO INVOLUCRA QUE INTEGREN TODO LO VISTO
-// ACLARACIÓN: ESTE DESAFÍO NO NECESARIAMENTE PODRÁ ESTAR RELACIONADO CON EL DESAFÍO FINAL
+function shop_proceeding() {
+    const user_name = prompt("Por favor, ingrese su nombre:");
+    user_welcoming(user_name);
 
-// CADA PUNTO EN REALIDAD ES UN EJERCICO ÚNICO QUE CONTIENE TODO LO NECESARIO PARA APROBAR
-// LOS TEXTOS DE LOS MENSAJES SON A CRITERIO PERSONAL, PERO DEBEN SEGUIR LA LÓGICA PLANTEADA
+    const product = get_product();
+    show_product_info(product);
 
-// 1) PRIMER CASO:
-// - Pregunte al usuario su nombre a través de un prompt. 
-// - Con el valor de su nombre, envíe un mensaje a través de un alert que le de la bienvenida. Ej: "Bienvenido a nuestro ecommerce Javier."
-// - Pregúntele al usuario, con un nuevo prompt, qué categoría de producto le interesaría comprar, ej: "¿Qué desea comprar? Elija la opción 1 si son remeras, opción 2 si son pantalones". La frase y la forma de introducir los datos queda a criterio de ustedes. Lo necesario es que a partir del valor ingresado, se valide qué mostrar luego.
-// - Valide el valor ingresado. En el caso de que sea, por ejemplo, 1, muestre por confirm el artículo remera, con un mensaje de este estilo: "Usted eligió remera. Su precio es de $x. Desea comprar este artículo?"
-// - En el caso de cancelar, terminar el algoritmo con un nuevo alert: "Muchas gracias por su visita"
-// - Si el usuario decide aceptar la compra, se deberá mediante un bucle, reproducir tres mensajes distintos: El primero será: "Ingrese su dirección para poder realizar el envío del pedido", el segundo: "Su artículo será enviado a: - dirección-" y el tercero: "Muchas gracias por su compra".
-// FIN DEL EJERCICIO
-// RECUERDEN ENCAPSULAR LA LÓGICA DENTRO DE FUNCIONES
-// PUEDEN HACER UNA FUNCIÓN QUE CONTENGA UN ALERT Y QUE EL TEXTO DEL ALERT SEA PASADO COMO PARÁMETRO, COSA DE NO REPETIR LOS ALERTS Y APRENDER A HACER LÓGICA REUTILIZABLE.
+    const confirmation = confirm("¿Desea comprar este artículo?");
+    if (confirmation) {
+        let adress;
+        do{
+            adress = prompt("Ingrese su dirección para poder realizar el envío del pedido:");
+        }while(adress == "");
 
-function mostrarBienvenida(user_name) {
+        alert(`Su articulo sera enviado a: ${adress}`);
+        alert("Muchas gracias por su compra, vuelva pronto!");
+    } else {
+        alert("Muchas gracias por su visita.");
+    }
+}
+
+function user_welcoming(user_name) {
     alert(`Bienvenido a nuestro ecommerce ${user_name}.`);
 }
 function get_product() {
@@ -38,25 +39,4 @@ function show_product_info(product) {
     }
 }
 
-function procesoCompra() {
-    const user_name = prompt("Por favor, ingrese su nombre:");
-    mostrarBienvenida(user_name);
-
-    const product = get_product();
-    show_product_info(product);
-
-    const confirmation = confirm("¿Desea comprar este artículo?");
-    if (confirmation) {
-        let adress;
-        do{
-            adress = prompt("Ingrese su dirección para poder realizar el envío del pedido:");
-        }while(adress == "");
-
-        alert(`Su articulo sera enviado a: ${adress}`);
-        alert("Muchas gracias por su compra, vuelva pronto!");
-    } else {
-        alert("Muchas gracias por su visita.");
-    }
-}
-
-procesoCompra();
+shop_proceeding();
